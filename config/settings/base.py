efac6a1e3f120be81c8e23342f6b44e63a62f7b4
seth <furman.seth@gmail.com>
2018-03-26 14:37:14 -0400

    complete tutorial: RESTful users api

--- config/settings/base.py
+++ config/settings/base.py
@@ -48,9 +48,11 @@ DJANGO_APPS = (
 )
 
 THIRD_PARTY_APPS = (
+    'rest_framework',
 )
 
 LOCAL_APPS = (
+    'project.api',
 )
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
@@ -149,3 +151,6 @@ STATICFILES_FINDERS = (
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = str(APPS_DIR('media'))
+
+REST_FRAMEWORK = {
+}
