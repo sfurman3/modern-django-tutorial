1ea45d9badfda0e9a8afb2e5bab14cedd8a44c6f
seth <furman.seth@gmail.com>
2018-04-02 10:55:35 -0400

    enable swagger app (API docs)

--- project/api/urls.py
+++ project/api/urls.py
@@ -2,9 +2,14 @@ from django.conf.urls import url, include
 from rest_framework import routers
 from project.api import views
 
+from rest_framework.schemas import get_schema_view
+
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 
+schema_view = get_schema_view(title='Users API')
+
 urlpatterns = [
     url(r'^', include(router.urls)),
+    url(r'^$', schema_view)
 ]
